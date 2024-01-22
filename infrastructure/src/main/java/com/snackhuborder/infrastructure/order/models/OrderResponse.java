99dc873950b141c8eccd466d9c667ab378635c7e
Gutemberg Cavalcante <gutembergrcc@gmail.com>
2024-01-21 22:29:57 -0300

    Build file

--- infrastructure/src/main/java/com/snackhuborder/infrastructure/order/models/OrderResponse.java
+++ infrastructure/src/main/java/com/snackhuborder/infrastructure/order/models/OrderResponse.java
@@ -9,6 +9,8 @@ public record OrderResponse (
         @JsonProperty("items") List<OrderItemResponse> items,
         @JsonProperty("orderIdentifier") String orderIdentifier,
         @JsonProperty("observation") String observation,
-        @JsonProperty("status") String status
+        @JsonProperty("status") String status,
+        @JsonProperty("timestamp") String timestamp
+
 ) {
 }
