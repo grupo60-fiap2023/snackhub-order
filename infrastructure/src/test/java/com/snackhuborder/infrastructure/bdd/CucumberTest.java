988592014ea1084766b618130f440cee237d1051
Gutemberg Cavalcante <gutembergrcc@gmail.com>
2024-01-21 22:53:51 -0300

    Build file

--- infrastructure/src/test/java/com/snackhuborder/infrastructure/bdd/CucumberTest.java
+++ infrastructure/src/test/java/com/snackhuborder/infrastructure/bdd/CucumberTest.java
@@ -3,9 +3,11 @@ package com.snackhuborder.infrastructure.bdd;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CucumberTest {
 }
